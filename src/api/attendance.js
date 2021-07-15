798feb748c39e4ff5131db43ffb479d7967e3d7f
atk-sz <65167734+atk-sz@users.noreply.github.com>
2021-07-15 12:41:02 +0530

    finally attendance part working

--- src/api/attendance.js
+++ src/api/attendance.js
@@ -11,3 +11,27 @@ export const createOrDisplayAttendance = async (authtoken, today, startToday) =>
         }
     );
 };
+
+export const markAbsent = async (aid, mid, authtoken) => {
+    return await axios.post(
+        `${process.env.REACT_APP_BACKEND_API}/attendance/${aid}/${mid}/absent`,
+        {},
+        {
+            headers: {
+                authtoken,
+            },
+        }
+    );
+};
+
+export const markPresent = async (aid, mid, authtoken) => {
+    return await axios.post(
+        `${process.env.REACT_APP_BACKEND_API}/attendance/${aid}/${mid}/present`,
+        {},
+        {
+            headers: {
+                authtoken,
+            },
+        }
+    );
+};
--- src/views/admin/Attendance.js
+++ src/views/admin/Attendance.js
@@ -1,19 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
-import { createOrDisplayAttendance } from '../../api/attendance';
+import { createOrDisplayAttendance, markAbsent, markPresent } from '../../api/attendance';
 import { AddMemberForm, AdminNav } from '../../components';
 
 const Attendance = () => {
     const { user } = useSelector(state => ({ ...state }))
     const [loading, setLoading] = useState(true)
+    const [members, setMembers] = useState([])
+    const [aid, setAid] = useState('')
     const startDate = new Date().setHours(0, 0, 0, 0)
     const startToday = new Date(startDate)
 
+    // useEffect(() => {
+    //     if (members.length)
+    //         console.log(members)
+    // }, [members])
+
+    const filterMember = ({ present, absent }) => {
+        return new Promise((resolve, reject) => {
+            let members = []
+            present.map(each => {
+                members.push({ user: each, attendance: true })
+            })
+            absent.map(each => {
+                members.push({ user: each, attendance: false })
+            })
+            resolve(members)
+        })
+    }
+
     useEffect(() => {
         createOrDisplayAttendance(user.token, new Date(), startToday)
-            .then(res => {
-                console.log(res.data)
+            .then(async res => {
+                setAid(res.data._id)
+                const resultMems = await filterMember(res.data)
+                setMembers(resultMems)
                 setLoading(false);
             })
             .catch((err) => {
@@ -22,6 +44,29 @@ const Attendance = () => {
                 console.log(err);
             });
     }, [])
+
+    async function handleToggle(e) {
+        try {
+            const updatedMembers = members.map((item, i) => {
+                if (this === i) {
+                    const updatedItem = {
+                        ...item,
+                        attendance: e.target.checked,
+                    };
+                    return updatedItem;
+                }
+                return item;
+            });
+
+            if (e.target.checked)
+                await markPresent(aid, members[this].user._id, user.token)
+            else
+                await markAbsent(aid, members[this].user._id, user.token)
+            setMembers(updatedMembers)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
 
     return (
         <div className="container-fluid admin-dashboard-div">
@@ -34,10 +79,20 @@ const Attendance = () => {
                         loading ? <h1>..loading</h1> : (
                             <>
                                 <h1>Attendance</h1>
-                                <div>
-
+                                <div className='attendance'>
+                                    {
+                                        members.length ? (members.map((each, i) => (
+                                            <div key={i} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} className="member-attendance">
+                                                <h5>{each.user.name}</h5>
+                                                <div className="form-check form-switch">
+                                                    <input className="form-check-input" type="checkbox" checked={each.attendance} onChange={handleToggle.bind(i)} />
+                                                </div>
+                                            </div>
+                                        ))
+                                        ) : ''
+                                    }
                                 </div>
-                                <AddMemberForm />
+                                {/* <AddMemberForm /> */}
                             </>
                         )
                     }
