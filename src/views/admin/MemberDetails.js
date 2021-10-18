ebde33cf654e61149d56629031c28c625aadd258
Syed Zaid <syed.zaid525@gmail.com>
2021-10-18 22:39:01 +0530

    made memebr id page

--- src/views/admin/MemberDetails.js
+++ src/views/admin/MemberDetails.js
@@ -9,12 +9,118 @@ import "./styles/members.css";
 import { Card, Row, Col, Container, Image } from "react-bootstrap";
 import { css } from "@emotion/react";
 import ScaleLoader from "react-spinners/ScaleLoader";
+import { CloseOutlined } from "@ant-design/icons";
+
+const IDCard = ({ member, setDisplay }) => {
+  const printableAreaRef = React.useRef(null);
+
+  const handlePrintClick = () => {
+    var mywindow = window.open("", "PRINT");
+    mywindow.document.write(printableAreaRef.current.innerHTML);
+    mywindow.document.close();
+    mywindow.focus();
+    mywindow.print();
+    return true;
+  };
+
+  return (
+    <div
+      style={{
+        left: "0",
+        top: "0",
+        width: "100vw",
+        height: "100vh",
+        position: "fixed",
+        zIndex: "2",
+        backgroundColor: "rgba(23,23,23,0.3)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      className="id-card-div"
+    >
+      <div
+        style={{
+          position: "relative",
+          backgroundColor: "white",
+          height: "80%",
+          width: "90%",
+        }}
+        className="id-card"
+      >
+        <CloseOutlined
+          style={{ position: "absolute", top: "10px", right: "10px" }}
+          onClick={() => setDisplay(false)}
+        />
+        <div
+          style={{
+            height: "90%",
+            width: "90%",
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+          ref={printableAreaRef}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+            }}
+            className="div-a"
+          >
+            <div className="img-div">
+              <img src={member.profile} alt={member.card_id} />
+            </div>
+            <div className="name-div">
+              <h3>
+                {member.fname} {member.lname}
+              </h3>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+            }}
+            className="div-b"
+          >
+            <div className="barcode-div">
+              <BarCode card_id={member.card_id} />
+            </div>
+            <div className="expire-div">
+              <p>
+                Expires:{" "}
+                {`${new Date(member.expire).getDate()}/${new Date(
+                  member.expire
+                ).getMonth()}/${new Date(member.expire).getFullYear()}`}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <button
+          style={{ position: "absolute", bottom: "20px", left: "50%" }}
+          className="btn btn-info"
+          onClick={handlePrintClick}
+        >
+          Print
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const MemberDetails = () => {
   const { mid } = useParams();
   const { user } = useSelector((state) => ({ ...state }));
   const [loading, setLoading] = useState(true);
   const [member, setMember] = useState({});
+  const [display, setDisplay] = useState(false);
 
   useEffect(() => {
     loadDetails();
@@ -63,7 +169,7 @@ const MemberDetails = () => {
 
   return (
     <Container className="member-detail-div">
-      <Row justifyContent="center">
+      <Row>
         <Col md="12">
           <Card>
             <Card.Body>
@@ -75,117 +181,134 @@ const MemberDetails = () => {
                   <ScaleLoader />
                 </div>
               ) : (
-                <Row className="member-details">
-                  <Col md={6} className="profile-barcode">
-                    <Image fluid src={member.profile} alt={member.card_id} />
-                    <BarCode card_id={member.card_id} />
-                  </Col>
-                  <Col md={6} className="member-details-area">
-                    <Card className="shadow-sm">
-                      <Card.Body>
-                        <div className="details-text-member">
-                          <h2>
-                            {member.fname} {member.lname}
-                          </h2>
-                        </div>
-                        <div className="details-text-member">
-                          <h4>{member.card_id}</h4>
-                        </div>
-                        <hr />
-                        <div className="info-category mt-5">
-                          <p>
-                            <small className="text-secondary">
-                              Personal Details
-                            </small>
-                          </p>
-                        </div>
-                        <div className="details-text-member">
-                          <div className="left-title">
-                            <div className="details-label-text">Age</div>
+                <>
+                  {member && display ? (
+                    <IDCard member={member} setDisplay={setDisplay} />
+                  ) : (
+                    ""
+                  )}
+                  <Row className="member-details">
+                    <Col md={6} className="profile-barcode">
+                      <Image fluid src={member.profile} alt={member.card_id} />
+                      <BarCode card_id={member.card_id} />
+                    </Col>
+                    <Col md={6} className="member-details-area">
+                      <Card className="shadow-sm">
+                        <Card.Body>
+                          <div className="details-text-member">
+                            <h2>
+                              {member.fname} {member.lname}
+                            </h2>
                           </div>
-                          <div className="details-info">
-                            {timeSince(member.DOB)}
+                          <div className="details-text-member">
+                            <h4>{member.card_id}</h4>
                           </div>
-                        </div>
-                        <div className="info-category mt-5">
-                          <p>
-                            <small className="text-secondary">
-                              Membership Details
-                            </small>
-                          </p>
-                        </div>
-                        <div className="details-text-member">
-                          <div className="left-title">
-                            <div className="details-label-text">House Id</div>
+                          <hr />
+                          <div className="info-category mt-5">
+                            <p>
+                              <small className="text-secondary">
+                                Personal Details
+                              </small>
+                            </p>
                           </div>
-                          <div className="member-detail-house-id">
-                            {member.house_id}
-                            <div>
-                              <Link
-                                to={`/admin/house/${member.house_id}/members`}
-                              >
-                                View all
-                              </Link>
+                          <div className="details-text-member">
+                            <div className="left-title">
+                              <div className="details-label-text">Age</div>
+                            </div>
+                            <div className="details-info">
+                              {timeSince(member.DOB)}
                             </div>
                           </div>
-                        </div>
-                        <div className="details-text-member">
-                          <div className="left-title">
-                            <div className="details-label-text">
-                              Member Since
+                          <div className="info-category mt-5">
+                            <p>
+                              <small className="text-secondary">
+                                Membership Details
+                              </small>
+                            </p>
+                          </div>
+                          <div className="details-text-member">
+                            <div className="left-title">
+                              <div className="details-label-text">House Id</div>
+                            </div>
+                            <div className="member-detail-house-id">
+                              {member.house_id}
+                              <div>
+                                <Link
+                                  to={`/admin/house/${member.house_id}/members`}
+                                >
+                                  View all
+                                </Link>
+                              </div>
                             </div>
                           </div>
-                          <div className="details-info">
-                            {timeSince(member.join)}
-                          </div>
-                        </div>
-                        <div className="details-text-member">
-                          <div className="left-title">
-                            <div className="details-label-text">Valid Till</div>
-                          </div>
-                          <div className="details-info">
-                            {`${new Date(member.expire).getDate()}/${new Date(
-                              member.expire
-                            ).getMonth()}/${new Date(
-                              member.expire
-                            ).getFullYear()}`}
-                          </div>
-                        </div>
-                        <div className="info-category mt-5">
-                          <p>
-                            <small className="text-secondary">
-                              Contact Details
-                            </small>
-                          </p>
-                        </div>
-                        <div className="details-text-member">
-                          <div className="left-title">
-                            <div className="details-label-text">Email</div>
-                          </div>
-                          <div className="details-info">{member.email}</div>
-                        </div>
-                        <div className="details-text-member">
-                          <div className="left-title">
-                            <div className="details-label-text">Phone No</div>
-                          </div>
-                          <div className="details-info">{member.phone}</div>
-                        </div>
-                        <div className="details-text-member">
-                          <div className="left-title">
-                            <div className="details-label-text">Address</div>
-                          </div>
-                          <div className="details-info">
-                            <div>{member.address.first_line}</div>
-                            <div>{member.address.second_line}</div>
-                            <div>
-                              {member.address.city} - {member.address.pincode}
+                          <div className="details-text-member">
+                            <div className="left-title">
+                              <div className="details-label-text">
+                                Member Since
+                              </div>
+                            </div>
+                            <div className="details-info">
+                              {timeSince(member.join)}
                             </div>
                           </div>
-                        </div>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                </Row>
+                          <div className="details-text-member">
+                            <div className="left-title">
+                              <div className="details-label-text">
+                                Valid Till
+                              </div>
+                            </div>
+                            <div className="details-info">
+                              {`${new Date(member.expire).getDate()}/${new Date(
+                                member.expire
+                              ).getMonth()}/${new Date(
+                                member.expire
+                              ).getFullYear()}`}
+                            </div>
+                          </div>
+                          <div className="info-category mt-5">
+                            <p>
+                              <small className="text-secondary">
+                                Contact Details
+                              </small>
+                            </p>
+                          </div>
+                          <div className="details-text-member">
+                            <div className="left-title">
+                              <div className="details-label-text">Email</div>
+                            </div>
+                            <div className="details-info">{member.email}</div>
+                          </div>
+                          <div className="details-text-member">
+                            <div className="left-title">
+                              <div className="details-label-text">Phone No</div>
+                            </div>
+                            <div className="details-info">{member.phone}</div>
+                          </div>
+                          <div className="details-text-member">
+                            <div className="left-title">
+                              <div className="details-label-text">Address</div>
+                            </div>
+                            <div className="details-info">
+                              <div>{member.address.first_line}</div>
+                              <div>{member.address.second_line}</div>
+                              <div>
+                                {member.address.city} - {member.address.pincode}
+                              </div>
+                            </div>
+                          </div>
+                          <button
+                            onClick={() => {
+                              setDisplay(true);
+                            }}
+                            className="btn btn-primary float-end"
+                          >
+                            Generate ID
+                          </button>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  </Row>
+                </>
               )}
             </Card.Body>
           </Card>
